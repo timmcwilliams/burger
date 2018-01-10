9aff65fc3577ac9365dfd47c2f409c2b613a6eb6
tmac4403 <drive911@gmail.com>
2018-01-10 12:46:59 -0600

    commit 2

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,10 @@
 CREATE DATABASE burgers_db;
 USE burgers_db;
+
 CREATE TABLE burgers (
-    id INTEGER(10) NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(50),
     devoured BOOLEAN DEFAULT false,
-    DATE date,
+    created_at NOT NULL DATETIME ,
     PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,9 @@
 use burgers_db;
-INSERT INTO burgers
-VALUES (1, "Cheese", 1, 2016-12-25);
-INSERT INTO burgers_db
-VALUES (2, "Bacon", 1, 2018-1-2);
-INSERT INTO burgers_db
-VALUES (3, "Goat Cheese", 1, 2016-10-10);
-INSERT INTO burgers_db
-VALUES (4, "Blue Cheese", 1, 2017-12-9);
-INSERT INTO burgers_db
-VALUES (5, "No-Cheese", 1, 2016-11-8);
+INSERT INTO burgers VALUES (1, "Cheese", true, "2016-12-25");
+INSERT INTO burgers VALUES (2, "Bacon", true, "2018-1-2");
+INSERT INTO burgers VALUES (3, "Goat Cheese", true, "2016-10-10");
+INSERT INTO burgers VALUES (4, "Blue Cheese", true, "2017-12-9");
+INSERT INTO burgers VALUES (5, "No-Cheese", true, "2016-11-8");
+
+
 
