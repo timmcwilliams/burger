13c8a4464cc73028809afa2a98b30830b890af81
tmac4403 <drive911@gmail.com>
2018-01-14 21:08:53 -0600

    try again

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 var mysql = require("mysql");
 
 var connection;
-if (process.env.burgers_db) {
-   connection = mysql.createConnection(process.env.TIMSDB_URL);
+if (process.env.JAWSDB_URL) {
+   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection= mysql.createConnection({
 
